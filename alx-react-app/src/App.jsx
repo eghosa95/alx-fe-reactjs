0e3f71308c66ebb927d0d90c9ed6fd2730e0271e
eghosa95 <iyobosaeghebiz@gmail.com>
2025-08-27 22:13:46 -0400

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,13 @@
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <WelcomeMessage />
-    </div>
-  )
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
-
-export default App
