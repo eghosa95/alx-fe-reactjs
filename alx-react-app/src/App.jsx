fa006e4624b41ee72794676eca051a60f805f222
eghosa95 <iyobosaeghebiz@gmail.com>
2025-08-27 22:17:54 -0400

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,19 @@
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
+    <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+
+      <UserProfile 
+        name="John Doe" 
+        age="30" 
+        bio="Enjoys coding, traveling, and coffee" 
+      />
+    </div>
   );
 }
